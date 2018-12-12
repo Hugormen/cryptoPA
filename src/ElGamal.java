112e4bc116c7c76a5220072c544d3af6048003df
Hugormen <hugo.ekelund@telia.com>
2018-12-12 11:05:49 +0100

    Done with ElGamal

--- src/ElGamal.java
+++ src/ElGamal.java
@@ -42,7 +42,45 @@ public class ElGamal {
     public static BigInteger recoverSecret(BigInteger p, BigInteger g,
                                            BigInteger y, int year, int month, int day, int hour, int minute,
                                            int second, BigInteger c1, BigInteger c2) {
-        return c1;
+
+        /**
+         * Find her random R by trying all different values of milliseconds.
+         */
+
+        BigInteger bigKey = BigInteger.ZERO;
+        for (int i = 0; i < 1000; i++) {
+            long key = recoverRandomKey(year, month, day, hour, minute, second,i);
+            bigKey = new BigInteger(Long.toString(key));
+
+            if (g.modPow(bigKey,p).compareTo(c1)==0){
+                System.out.println(i);
+                System.out.println(bigKey);
+                break;
+            }
+        }
+        /**
+         * Since we have h (which is y here), the size of the group (which is p)
+         * , aswell as r and c2,
+         * we can use the second part of ElGamal-encryption to
+         * get out the m.
+         *
+         * c2 = m * h^r (mod p)
+         *
+         * This gives us
+         *
+         * m = (c2 * (h^r)^-1) (mod p)
+         */
+
+        BigInteger hr = y.modPow(bigKey,p);
+        BigInteger hrInv = hr.modInverse(p);
+
+        BigInteger m = c2.multiply(hrInv);
+
+        return m.mod(p);
     }
 
+    private static long recoverRandomKey(int year, int month, int day, int hour, int minute, int second,int mili){
+        return (long)(year*(Math.pow(10,10))+month*(Math.pow(10,8))+day*(Math.pow(10,6))+hour*(Math.pow(10,4))+
+                minute*(Math.pow(10,2))+second + mili);
+    }
 }
